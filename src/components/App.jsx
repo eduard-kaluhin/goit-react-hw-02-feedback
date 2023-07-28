2ca96663dba4625433bf4cb180a8167786b03d70
eduard-kaluhin <renoserviser@gmail.com>
2023-07-28 12:15:36 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,35 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+
+import Section from './Section';
+import FeedbackOptions from './FeedbackOptions';
+import Statistics from './Statistics';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  componentDidMount() {
+    document.title = 'HW-2 Feedback';
+  }
+
+  onClick = option => {
+    this.setState(prevState => ({
+      [option]: prevState[option] + 1,
+    }));
+  };
+
+  render() {
+    return (
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          onClick={this.onClick}
+          feedbackOptions={this.state}
+        ></FeedbackOptions>
+        <Statistics feedbackOptions={this.state} />
+      </Section>
+    );
+  }
+}
