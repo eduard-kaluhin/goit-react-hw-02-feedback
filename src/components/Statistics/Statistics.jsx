e509df0cdb8ae270237fff1776554b227b41b429
eduard-kaluhin <renoserviser@gmail.com>
2023-08-05 15:28:34 +0300

    Update Statistics.jsx

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,25 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
 import { Box, Grid, ResultWrapper } from './Statistics.styled';
 import StatisticsLayout from '../StatisticsLayout';
-
-const Statistics = ({ feedbackOptions }) => {
+import Notification from '../Notification';
+const Statistics = ({ feedbackOptions, totalFeedback, positivePercentage }) => {
   const { good, neutral, bad } = feedbackOptions;
-  const optionsKey = Object.keys(feedbackOptions);
-
-  const totalFeedback = good + neutral + bad;
-  const positivePercentage =
-    totalFeedback === 0 ? 0 : Math.round((good / totalFeedback) * 100);
-
   return (
     <StatisticsLayout title="Statistics">
-      {totalFeedback === 0 ? (
-        <p>No feedback given</p>
-      ) : (
         <Box>
           <Grid>
-            {optionsKey.map(key => {
+            {Object.keys(feedbackOptions).map(key => {
               return (
                 <div className="option-wrapper" key={key}>
                   <p className="option-name">{key}</p>
@@ -35,17 +25,17 @@ const Statistics = ({ feedbackOptions }) => {
             <p>Positive feedback: {positivePercentage}%</p>
           </ResultWrapper>
         </Box>
-      )}
     </StatisticsLayout>
   );
 };
-
 Statistics.propTypes = {
   feedbackOptions: PropTypes.shape({
     good: PropTypes.number.isRequired,
     neutral: PropTypes.number.isRequired,
     bad: PropTypes.number.isRequired,
   }),
+  totalFeedback: PropTypes.number.isRequired,
+  positivePercentage: PropTypes.number.isRequired,
 };
 
 export default Statistics;
